208d6efb85a9bd1c0de186e1cfccf55cc20c4b97
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-06-09 16:57:55 -0400

    cpp folder

---
+++ cpp-basic/input-output.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+  // input containers
+  int n1, n2, n3;
+
+  // write stdin to n
+  cin >> n1;
+  cin >> n2;
+  cin >> n3;
+
+  cout << n1 + n2 + n3 << endl;
+
+  return 0;
+}
