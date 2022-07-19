36ef612d64a5a127e6e77224bd0999237fde39d3
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-07-18 22:33:07 -0400

    Update for-loops.cpp

--- cpp-basic/for-loops.cpp
+++ cpp-basic/for-loops.cpp
@@ -29,31 +29,26 @@ string numbers[] = {
   "six", "seven", "eight", "nine"
   };
   
-string isEven(int n)
-{
-  return (n % 2 == 0) ? "even" : "odd";
-}  
+/** Returns string if n is even or odd */  
+string isEven(int n) { return (n % 2 == 0) ? "even" : "odd"; }  
   
-void numberTranslate(int n) 
+void numberTranslate(int n1, int n2) 
 {  
-  for(int i = 0; i < 10; i++)
+  for(int i = n1; i <= n2; i++)
   { 
-    if(n <= i) cout << numbers[i-1] << endl;
-    else if(n > 9) cout << isEven(i) << endl;
+    if(i <= 9) cout << numbers[i-1] << endl;
+    else if(i > 9) cout << isEven(i) << endl;
   }
-  cout << "done\n";
 }  
 
 int main() {
-  /* Enter your code here. 
-  Read input from STDIN. Print output to STDOUT */
-  
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
   int n1, n2;
   cin >> n1;
   cin >> n2;
   
-  numberTranslate(n1);
-  numberTranslate(n2);
-
+  numberTranslate(n1, n2);
+  
   return 0;
 }
